55d37693663061c8c80a3dbc41436f0850bc29fb
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-08-30 22:56:02 -0400

    Game over alert, rounds for new enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,45 +31,57 @@ var fight = function(enemyName) {
                 break;
             }
             // if no ask again by running fight().
-            }
-            //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-            enemyHealth = enemyHealth - playerAttack;
-            // Log a resulting message to the console so we know that it worked.
-           console.log(
-               playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-           );
+        }
+        //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+        enemyHealth = enemyHealth - playerAttack;
+        // Log a resulting message to the console so we know that it worked.
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
     
-            // check enemy health.
-            if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
-            playerMoney = playerMoney + 20;
-            break;
-            }
-            else {
-                window.alert(enemyName + " still has " + enemyHealth + " health remaining.");
-            }
+    // check enemy health.
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+        playerMoney = playerMoney + 20;
+        break;
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health remaining.");
+    }
         
-            // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-            playerHealth = playerHealth - enemyAttack;
-            // Log a resulting message to the console so we know that it worked.
-            console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-            );
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
         
-            // Check player health.
-            if (playerHealth <=0) {
-                window.alert(playerName + " has died!");
-                break;
-            } else {
-                window.alert(playerName + " still has " + playerHealth + " health remaining.");
-            };
-        };   
+    // Check player health.
+    if (playerHealth <=0) {
+        window.alert(playerName + " has died!");
+        break;
+    } else {
+        window.alert(playerName + " still has " + playerHealth + " health remaining.");
+    };
+    };   
+};
+
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            // pick new enemy to fight based on index of enemyNames array
+            var pickedEnemyName = enemyNames[i];
+            // reset enemyHealth before starting new fight
+            enemyHealth = 50;
+            // use debugger to pause script from running and check what's going on at that moment in the code
+            // debugger;
+        
+            // pass the pickedEnemyName variable's value into the fight function, where it will assume the name of the enemyName parameter
+            fight(pickedEnemyName);
+        } else {
+            window.alert("You have lost your robot in battle! Game Over!");
+        }
     };
 
-    
-for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-}
-
+    // var pickedEnemyName = enemyNames[i];
+    // enemyHealth = 50;
+    // fight(pickedEnemyName);
