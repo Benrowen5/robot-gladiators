266038d1c40b774e3a80876b1f33326fd9fa88eb
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-09-01 18:21:26 -0400

    bug fix for blank or null name

--- assets/js/game.js
+++ assets/js/game.js
@@ -138,8 +138,17 @@ var randomNumber = function(max, min) {
     return value;
 };
 
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null){
+        name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -182,5 +191,4 @@ var enemyInfo = [
     }
 ];
 
-// debugger;
 startGame();
